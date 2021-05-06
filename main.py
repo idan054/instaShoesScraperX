9df61741ff9daceaa521230d9a9914ebbd84ed6c
idan054 <idanbit80@gmai.com>
2021-05-06 16:19:49 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import gspread
+
+gc = gspread.service_account(filename="cerdi.json")
+
+sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/12xTW6jLIQ1yx538eo3bQ1veDd8zhItJ3oImINvOEaUE/edit?usp=sharing')
+# sh = gc.open_by_key('12xTW6jLIQ1yx538eo3bQ1veDd8zhItJ3oImINvOEaUE')
+
+# Most common case: Sheet1
+worksheet = sh.sheet1
+
+# all_col_values_lists = worksheet.get_all_values()
+# print(*all_col_values_lists, sep="\n")
+
+# Get next empty row
+colA_values = worksheet.col_values(1)
+empty_row = len(colA_values)+1
+
+# Update a range
+worksheet.update(f'A{empty_row}:Z{empty_row}', [
+    ["A", "B"],
+])
